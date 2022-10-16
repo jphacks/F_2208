b35b307a69ab4cb44f42c10085bab15af745efa3
sakai taigo <200442066@ccmailg.meijo-u.ac.jp>
2022-10-17 02:20:57 +0900

    add:make Friend and Task table

---
+++ backend/database/migrations/2022_10_16_170307_create_tasks_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tasks', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->string('description')->nullable();
+            $table->integer('exp')->default(0);
+            $table->timestamp('time_limit')->nullable();
+            $table->tinyinteger('severity')->default(0);
+            $table->tinyinteger('status')->default(1);
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->foreign('order_user_id')->references('id')->on('users')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tasks');
+    }
+};
