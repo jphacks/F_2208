3f928728aa0ee0094f7c8b979970841694992339
yuuumbk <hikoki.y@gmail.com>
2022-10-16 14:30:47 +0900

    add: ユーザ作成処理のログを取得する

--- backend/app/Actions/Fortify/CreateNewUser.php
+++ backend/app/Actions/Fortify/CreateNewUser.php
@@ -18,6 +18,7 @@ class CreateNewUser implements CreatesNewUsers {
      * @return \App\Models\User
      */
     public function create(array $input) {
+        \Log::info($input);
         Validator::make($input, [
             'name' => ['required', 'string', 'max:255'],
             'email' => [
