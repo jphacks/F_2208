8d59d95b861ce07bcd69263c83d2da2d077e7c58
yuuumbk <hikoki.y@gmail.com>
2022-10-17 16:58:31 +0900

    add: Added a button to test the authentication API

--- frontend/src/pages/Top.jsx
+++ frontend/src/pages/Top.jsx
@@ -1,5 +1,25 @@
+import { Button } from "@mui/material";
+import login from "../api/login";
+import register from "../api/register";
+import axios from "../libs/axios";
+
 const Top = () => {
-  return <>Top Page</>;
+  const name = "email";
+  const email = "test@exapmle.com";
+  const password = "sdafaw3970rpaiwou";
+
+  const handleSubmitRegister = () => {
+    axios.get("/api/user").then((res) => console.log(res));
+  };
+  return (
+    <>
+      <Button onClick={handleSubmitRegister}>ユーザ</Button>
+      <Button onClick={() => register(name, email, password, password)}>
+        登録
+      </Button>
+      <Button onClick={() => login(email, password)}>ログイン</Button>
+    </>
+  );
 };
 
 export default Top;
