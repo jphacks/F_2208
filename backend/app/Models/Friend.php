1f99eab9eb795edea25add12468c8c818f9c520a
sakai taigo <200442066@ccmailg.meijo-u.ac.jp>
2022-10-17 21:33:16 +0900

    add:disable primary key

--- backend/app/Models/Friend.php
+++ backend/app/Models/Friend.php
@@ -20,4 +20,7 @@ class Friend extends Model
     public function user() {
         return $this->belongsTo(User::class);
     }
+
+    protected $primaryKey = null;
+    public $incrementing = false;
 }
