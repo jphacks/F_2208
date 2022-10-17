cc72c47f5a2d66cbc8c3fc63f98e0acf2ec90b3f
sakai taigo <200442066@ccmailg.meijo-u.ac.jp>
2022-10-17 20:12:14 +0900

    add:Friend class model

--- backend/app/Models/Friend.php
+++ backend/app/Models/Friend.php
@@ -8,4 +8,15 @@ use Illuminate\Database\Eloquent\Model;
 class Friend extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'intimacy',
+        'favorite',
+        'sent_exp',
+        'recieved_exp',
+    ];
+
+    public function user() {
+        return $this->belongsTo(User::class);
+    }
 }
