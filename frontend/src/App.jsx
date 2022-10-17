ccf06b4498a663f288331dba0dffa93626e3c48d
yuuumbk <hikoki.y@gmail.com>
2022-10-17 17:57:34 +0900

    add: routing #22

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,11 @@
 import { QueryClient, QueryClientProvider } from "react-query";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Dashboard from "./pages/Dashboard";
+import NotFound from "./pages/NotFound";
+import Tasks from "./pages/Tasks";
 import Top from "./pages/Top";
+import User from "./pages/User";
+import UserSettings from "./pages/UserSettings";
 
 const queryClient = new QueryClient();
 
@@ -9,7 +14,12 @@ const App = () => {
     <QueryClientProvider client={queryClient}>
       <BrowserRouter>
         <Routes>
-          <Route path="/" exact element={<Top />} />
+          <Route index element={<Top />} />
+          <Route path="/dashboard" exact element={<Dashboard />} />
+          <Route path="/users/:userId" element={<User />} />
+          <Route path="/users/:userId/settings" element={<UserSettings />} />
+          <Route path="/tasks" element={<Tasks />} />
+          <Route path="*" element={<NotFound />} />
         </Routes>
       </BrowserRouter>
     </QueryClientProvider>
