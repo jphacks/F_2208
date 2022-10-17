25c8d94ce3cb086b9b979460b5aa51cabbdc69aa
sakai taigo <200442066@ccmailg.meijo-u.ac.jp>
2022-10-17 19:00:44 +0900

    add:generate task model

--- backend/app/Models/Task.php
+++ backend/app/Models/Task.php
@@ -8,4 +8,18 @@ use Illuminate\Database\Eloquent\Model;
 class Task extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'title',
+        'exp',
+        'severity',
+        'status',
+    ];
+
+    public function usertask() {
+        return $this->belongsTo(User::class);
+    }
+    public function orderuser() {
+        return $this->belongsTo(User::class);
+    }
 }
