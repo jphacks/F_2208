8e5f6783e0b5b0ed65818e7e00450db02857e99a
yuuumbk <hikoki.y@gmail.com>
2022-10-18 15:02:22 +0900

    add: tasks api client

---
+++ frontend/src/api/task.js
@@ -0,0 +1,89 @@
+import axios from "../libs/axios";
+
+export const fetchTasks = async () => {
+  return await axios
+    .get("/api/tasks")
+    .then((res) => {
+      console.log("[tasks]取得成功");
+      return res;
+    })
+    .catch((error) => {
+      console.log("[tasks]取得失敗");
+      console.log(error);
+      return error;
+    });
+}
+
+export const fetchTask = async (id) => {
+  return await axios
+    .get(`/api/tasks/${id}`)
+    .then((res) => {
+      console.log("[task]取得成功");
+      return res;
+    })
+    .catch((error) => {
+      console.log("[task]取得失敗");
+      console.log(error);
+      return error;
+    });
+}
+
+export const createTask = async (title, description, exp, time_limit, severity, status, user_id, order_user_id) => {
+  return await axios
+    .post(`/api/tasks`, {
+      title,
+      description,
+      exp,
+      time_limit,
+      severity,
+      status,
+      user_id,
+      order_user_id
+    })
+    .then((res) => {
+      console.log("[task]作成成功");
+      return res;
+    })
+    .catch((error) => {
+      console.log("[task]作成失敗");
+      console.log(error);
+      return error;
+    });
+}
+
+export const updateTask = async (id, title, description, exp, time_limit, severity, status, user_id, order_user_id) => {
+  return await axios
+    .put(`/api/tasks/${id}`, {
+      title,
+      description,
+      exp,
+      time_limit,
+      severity,
+      status,
+      user_id,
+      order_user_id
+    })
+    .then((res) => {
+      console.log("[task]更新成功");
+      return res;
+    })
+    .catch((error) => {
+      console.log("[task]更新失敗");
+      console.log(error);
+      return error;
+    });
+}
+
+export const deleteTask = async (id) => {
+  return await axios
+    .delete(`/api/tasks/${id}`)
+    .then((res) => {
+      console.log("[task]削除成功")
+      return res;
+    })
+    .catch((error) => {
+      console.log("[task]削除失敗")
+      console.log(error);
+      return error;
+    })
+}
