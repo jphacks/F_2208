669a2d296557994497d61edecfc13bc6891a170f
yuuumbk <hikoki.y@gmail.com>
2022-10-21 22:06:10 +0900

    add: user profile page design

--- frontend/src/components/User/UserStatus.jsx
+++ frontend/src/components/User/UserStatus.jsx
@@ -1,7 +1,7 @@
 import { useContext } from "react";
 import { userContext } from "../../contexts/userContext";
 import { css } from "@emotion/react";
-import { Box, Stack } from "@mui/material";
+import { Box, Stack, Typography } from "@mui/material";
 import LevelExpBar from "./LevelExpBar";
 
 const UserStatus = () => {
@@ -15,8 +15,70 @@ const UserStatus = () => {
         padding: 1em 1.3em;
         margin: 0 auto;
       `}
+      spacing={2}
     >
       <LevelExpBar />
+      <Box>
+        <Typography variant="h6" color="inherit">
+          トータルEXP
+        </Typography>
+        <Stack
+          direction="row"
+          alignItems="baseline"
+          spacing={1.5}
+          justifyContent="end"
+          css={css`
+            padding-right: 20px;
+          `}
+        >
+          <Typography variant="h4">
+            {user.total_exp.toLocaleString()}
+          </Typography>
+          <Typography variant="body1" color="inherit">
+            EXP
+          </Typography>
+        </Stack>
+      </Box>
+      <Box>
+        <Typography variant="h6" color="inherit">
+          貯金ポイント
+        </Typography>
+        <Stack
+          direction="row"
+          alignItems="baseline"
+          spacing={1.5}
+          justifyContent="end"
+          css={css`
+            padding-right: 20px;
+          `}
+        >
+          <Typography variant="h4">{user.point.toLocaleString()}</Typography>
+          <Typography variant="body1" color="inherit">
+            pt
+          </Typography>
+        </Stack>
+      </Box>
+      <Box>
+        <Typography variant="h6" color="inherit">
+          出金可能ポイント
+        </Typography>
+        <Stack
+          direction="row"
+          alignItems="baseline"
+          spacing={1.5}
+          justifyContent="end"
+          css={css`
+            padding-right: 20px;
+          `}
+        >
+          <Typography variant="h4">
+            {user.balance_exp.toLocaleString()}
+          </Typography>
+          <Typography variant="body1" color="inherit">
+            pt
+          </Typography>
+        </Stack>
+      </Box>
     </Stack>
   );
 };
