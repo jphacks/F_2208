b0a9833d9ada07b799a2a661928827afed4e7509
yuuumbk <hikoki.y@gmail.com>
2022-10-22 00:10:27 +0900

    add: point component design

--- frontend/src/components/Point.jsx
+++ frontend/src/components/Point.jsx
@@ -1,7 +1,7 @@
 import ShadowBox from "../components/ShadowBox";
 import RewardBg from "../assets/img/RewardBg.png";
 import { css } from "@emotion/react";
-import { Box, Typography } from "@mui/material";
+import { Box, Stack, Typography } from "@mui/material";
 import { useContext } from "react";
 import { userContext } from "../contexts/userContext";
 
@@ -9,68 +9,66 @@ const Point = () => {
   const { user, setUser } = useContext(userContext);
 
   return (
-    <ShadowBox>
+    <ShadowBox bgColor="#ffffee">
       <Box
         css={css`
           height: 350px;
-          /* padding-top: 1vw; */
-          padding-bottom: 0vw;
+          margin-top: 30px;
           background-image: url(${RewardBg});
           background-position: 50% 50%;
           background-color: rgba(255, 255, 255, 0.2);
           background-blend-mode: lighten;
-          background-size: 100%;
-          @media screen and (max-width: 600px) {
-            background-size: 80vw;
-          }
-          /* width: 100; */
-          /* display: flex;
+          background-size: 120%;
+          background-repeat: none;
+          display: flex;
           align-items: center;
-          flex-direction: column; */
+          justify-content: center;
+          flex-direction: column;
+          @media screen and (max-width: 600px) {
+            background-size: 85vw;
+          }
         `}
       >
-        <div
+        <Stack
+          spacing={2}
           css={css`
-            padding-bottom: 10px;
             text-align: center;
           `}
         >
-          <h1
+          <Typography
+            variant="h5"
             css={css`
-              color: red;
-              font-size: 60px;
-              @media screen and (max-width: 600px) {
-                font-size: 10vw;
-                margin-bottom: 10px;
-              }
+              letter-spacing: 0.01em;
             `}
           >
-            {user?.balance_exp}
-          </h1>
-          <h1
-            css={css`
-              margin-top: 0;
-              margin-bottom: 20px;
-              font-size: 30px;
-              @media screen and (max-width: 600px) {
-                background-size: 8vw;
-              }
-            `}
-          >
-            pt
-          </h1>
-          <h3
-            css={css`
-              font-size: 1.5vw;
-              @media screen and (min-width: 600px) {
-                font-size: 15px;
-                margin-bottom: 0;
-              }
-            `}
-          >
-            Amazonギフト券3,000円まで<br></br>あと150pt!!
-          </h3>
-        </div>
+            トータルEXP
+          </Typography>
+          <Stack spacing={0.5}>
+            <Typography
+              variant="h3"
+              css={css`
+                color: #f00;
+                letter-spacing: 0.01em;
+              `}
+            >
+              {user?.balance_exp}
+            </Typography>
+            <Typography variant="h4">Exp</Typography>
+          </Stack>
+          <Typography variant="h6">
+            {3000 - user?.balance_exp > 0 ? (
+              <>
+                ギフト券3,000円まで<br></br>あと {3000 - user?.balance_exp}pt!!
+              </>
+            ) : (
+              <>
+                すごい！
+                <br />
+                この調子で頑張ろう！
+              </>
+            )}
+          </Typography>
+        </Stack>
       </Box>
     </ShadowBox>
   );
