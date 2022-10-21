550cd3ba8fa062784fff5f7a2159db44816be09a
yuuumbk <hikoki.y@gmail.com>
2022-10-22 08:38:28 +0900

    add: loading animation

--- frontend/src/components/Task/ShowTasks.jsx
+++ frontend/src/components/Task/ShowTasks.jsx
@@ -15,12 +15,14 @@ import ShadowBox from "../ShadowBox";
 import { Link } from "react-router-dom";
 import { fetchFriendUsers } from "../../api/friend";
 import CompareArrowsIcon from "@mui/icons-material/CompareArrows";
+import PigImage from "../../assets/img/pig.png";
 
 export const ShowTasks = ({
   limited = false,
   showMore = false,
   hideDoneTask = false,
 }) => {
+  const [isLoading, setIsLoading] = useState(true);
   const { user, setUser } = useContext(userContext);
   const [open, setOpen] = useState(false);
   const [myTasks, setMyTasks] = useState();
@@ -33,12 +35,12 @@ export const ShowTasks = ({
 
   useEffect(() => {
     (async () => {
-      const res = await fetchTasks();
-      setMyTasks(res.data);
-    })();
-    (async () => {
-      const res = await fetchOrderedTasks();
-      setOrderedTasks(res.data);
+      const resMyTasks = await fetchTasks();
+      setMyTasks(resMyTasks.data);
+      const resOrderedTasks = await fetchOrderedTasks();
+      setOrderedTasks(resOrderedTasks.data);
+
+      setIsLoading(false);
     })();
   }, []);
   const [checked, setChecked] = useState([0]);
@@ -104,6 +106,125 @@ export const ShowTasks = ({
       }
     })();
   }, [user]);
+
+  if (isLoading) {
+    // if (true) {
+    return (
+      <ShadowBox>
+        <Box
+          css={css`
+            padding: 30px 10px;
+            height: 40vh;
+          `}
+        >
+          <Stack
+            spacing={2}
+            css={css`
+              height: 100%;
+            `}
+          >
+            <Grid container alignItems="center">
+              <Grid item xs={2}></Grid>
+              <Grid item xs={8}>
+                <Typography variant="h5" align="center">
+                  マイタスク
+                </Typography>
+              </Grid>
+              <Grid item xs={2} align="end">
+                <CompareArrowsIcon
+                  fontSize="large"
+                  css={css`
+                    color: #444;
+                    &:hover {
+                      cursor: pointer;
+                    }
+                  `}
+                />
+              </Grid>
+            </Grid>
+            <Stack
+              spacing={2}
+              divider={<Divider />}
+              css={css`
+                height: 100%;
+              `}
+            >
+              <Button
+                variant="contained"
+                css={css`
+                  background-color: #f67690;
+                  opacity: 0.8;
+                  &:hover {
+                    background-color: #dc8ba7;
+                  }
+                `}
+              >
+                タスクを追加する
+              </Button>
+              <Box
+                css={css`
+                  display: flex;
+                  justify-content: center;
+                  align-items: center;
+                  height: 100%;
+                `}
+              >
+                <img
+                  src={PigImage}
+                  css={css`
+                    display: block;
+                    width: 45%;
+                    @media screen and (min-width: 768px) {
+                      width: 35%;
+                    }
+                    animation-name: korokoro;
+                    animation-duration: 1s;
+                    animation-timing-function: linear;
+                    animation-iteration-count: infinite;
+                    @keyframes korokoro {
+                      0% {
+                        transform: translate(0%, 0%);
+                      }
+                      5% {
+                        transform: translate(10%, 0%) rotate(10deg);
+                      }
+                      25% {
+                        transform: translate(20%, 0%) rotate(20deg);
+                      }
+                      30% {
+                        transform: translate(-10%, 0%) rotate(-10deg);
+                      }
+                      35% {
+                        transform: translate(-15%, 0%) rotate(-15deg);
+                      }
+                      45% {
+                        transform: translate(10%, 0%) rotate(10deg);
+                      }
+                      50% {
+                        transform: translate(15%, 0%) rotate(15deg);
+                      }
+                      60% {
+                        transform: translate(-5%, 0%) rotate(-5deg);
+                      }
+                      65% {
+                        transform: translate(-7%, 0%) rotate(-7deg);
+                      }
+                      75% {
+                        transform: translate(0%, 0%) rotate(0deg);
+                      }
+                      100% {
+                        transform: translate(0%, 0%) rotate(0deg);
+                      }
+                    }
+                  `}
+                />
+              </Box>
+            </Stack>
+          </Stack>
+        </Box>
+      </ShadowBox>
+    );
+  }
 
   if (!!myTasks && !!orderedTasks) {
     return (
--- frontend/src/components/Task/TaskItem.jsx
+++ frontend/src/components/Task/TaskItem.jsx
@@ -40,199 +40,206 @@ const TaskItem = ({
   };
 
   return (
-    <Grid
-      key={task.id}
-      container
-      alignItems="center"
-      css={css`
-        &:hover {
-          cursor: pointer;
-        }
-      `}
-    >
-      <Grid item xs={2}>
-        <Checkbox
-          edge="end"
-          onChange={() => handleToggleCheckbox(task)}
-          checked={task.status === 2 || checked.indexOf(task.id) !== -1}
-          inputProps={{ "aria-labelledby": task.title }}
-          css={css`
-            &.Mui-checked {
-              color: #fe3e83;
-            }
-          `}
-        />
-      </Grid>
-      <Grid
-        item
-        xs={!showMyTasks || task.order_user_id === user?.id ? 8 : 10}
-        onClick={handleClickOpen}
-      >
-        <Stack spacing={0.5}>
-          <Stack direction="row" spacing={0.5} alignItems="center">
-            {showMyTasks && user && task.order_user.id !== user.id && (
-              <>
-                <Avatar
-                  src={PigImage}
-                  css={css`
-                    width: 18px;
-                    height: 18px;
-                  `}
-                />
-                <Typography variant="caption">
-                  {task.order_user.name} さんがタスクを割り当てました
-                </Typography>
-              </>
-            )}
-            {!showMyTasks && (
-              <>
-                <Avatar
-                  src={PigImage}
-                  css={css`
-                    width: 18px;
-                    height: 18px;
-                  `}
-                />
-                <Typography variant="caption">
-                  {task.user.name} さんへタスクを割り当てました
-                </Typography>
-              </>
-            )}
-          </Stack>
-          <Typography
-            variant="h6"
-            css={css`
-              color: ${task.severity > 2
-                ? "#d50000"
-                : task.severity > 1
-                ? "#ff8f00"
-                : "#212121"};
-            `}
-          >
-            {task.title}
-          </Typography>
-          <Grid container>
-            <Grid item xs={12} md={6}>
-              <Typography
-                variant="body2"
-                color="text.primary"
+    <Grid container>
+      <Grid item xs={2}></Grid>
+      <Grid item xs={10}>
+        <Stack direction="row" spacing={0.5} alignItems="center">
+          {showMyTasks && user && task.order_user.id !== user.id && (
+            <>
+              <Avatar
+                src={PigImage}
                 css={css`
-                  display: flex;
-                  justify-content: start;
-                  align-items: center;
-                  gap: 0.5em;
+                  width: 18px;
+                  height: 18px;
                 `}
-              >
-                <ControlPointDuplicateIcon fontSize="small" />
-                <span>
-                  {task.exp}
-                  <span
-                    css={css`
-                      font-size: 0.6em;
-                    `}
-                  >
-                    {" "}
-                    Exp
-                  </span>
-                </span>
+              />
+              <Typography variant="caption">
+                {task.order_user.name} さんが割り当てました
               </Typography>
-            </Grid>
-            <Grid item xs={12} md={6}>
-              <Typography
-                variant="body2"
-                color="text.primary"
+            </>
+          )}
+          {!showMyTasks && (
+            <>
+              <Avatar
+                src={PigImage}
                 css={css`
-                  display: flex;
-                  justify-content: start;
-                  align-items: center;
-                  gap: 0.5em;
+                  width: 18px;
+                  height: 18px;
                 `}
-              >
-                <AccessTimeIcon fontSize="small" />
-                {task.time_limit ? `${changeJSTDate(task.time_limit)}` : "なし"}
+              />
+              <Typography variant="caption">
+                {task.user.name} さんへ割り当てました
               </Typography>
-            </Grid>
-          </Grid>
+            </>
+          )}
         </Stack>
       </Grid>
-      {(!showMyTasks || task.order_user_id === user?.id) && (
+      <Grid
+        key={task.id}
+        container
+        alignItems="center"
+        css={css`
+          &:hover {
+            cursor: pointer;
+          }
+        `}
+      >
         <Grid item xs={2}>
-          <Stack
-            spacing={0.5}
+          <Checkbox
+            edge="end"
+            onChange={() => handleToggleCheckbox(task)}
+            checked={task.status === 2 || checked.indexOf(task.id) !== -1}
+            inputProps={{ "aria-labelledby": task.title }}
             css={css`
-              align-items: end;
-              padding-right: 10px;
+              &.Mui-checked {
+                color: #fe3e83;
+              }
             `}
-          >
-            <>
-              <EditTask
-                setTasks={setTasks}
-                task={task}
-                friends={friends}
-                setFriends={setFriends}
-                showMyTasks={showMyTasks}
-              />
-              <DeleteTask
-                setTasks={setTasks}
-                id={task.id}
-                showMyTasks={showMyTasks}
-              />
-            </>
+          />
+        </Grid>
+        <Grid
+          item
+          xs={!showMyTasks || task.order_user_id === user?.id ? 8 : 10}
+          onClick={handleClickOpen}
+        >
+          <Stack spacing={0.5}>
+            <Typography
+              variant="h6"
+              css={css`
+                color: ${task.severity > 2
+                  ? "#d50000"
+                  : task.severity > 1
+                  ? "#ff8f00"
+                  : "#212121"};
+              `}
+            >
+              {task.title}
+            </Typography>
+            <Grid container>
+              <Grid item xs={12} md={6}>
+                <Typography
+                  variant="body2"
+                  color="text.primary"
+                  css={css`
+                    display: flex;
+                    justify-content: start;
+                    align-items: center;
+                    gap: 0.5em;
+                  `}
+                >
+                  <ControlPointDuplicateIcon fontSize="small" />
+                  <span>
+                    {task.exp}
+                    <span
+                      css={css`
+                        font-size: 0.6em;
+                      `}
+                    >
+                      {" "}
+                      Exp
+                    </span>
+                  </span>
+                </Typography>
+              </Grid>
+              <Grid item xs={12} md={6}>
+                <Typography
+                  variant="body2"
+                  color="text.primary"
+                  css={css`
+                    display: flex;
+                    justify-content: start;
+                    align-items: center;
+                    gap: 0.5em;
+                  `}
+                >
+                  <AccessTimeIcon fontSize="small" />
+                  {task.time_limit
+                    ? `${changeJSTDate(task.time_limit)}`
+                    : "なし"}
+                </Typography>
+              </Grid>
+            </Grid>
           </Stack>
         </Grid>
-      )}
-      <Grid
-        container
-        css={css`
-          max-height: ${show ? "100vh" : 0};
-          transition: max-height 0.7s, padding 0.5s;
-          overflow: hidden;
-          padding: ${show ? "20px 0 0" : 0};
-        `}
-        onClick={handleClickOpen}
-      >
-        <Grid item xs={2}></Grid>
-        <Grid item xs={10}>
-          <Stack spacing={3}>
-            <Stack spacing={0.3}>
-              <Typography variant="body2" color="text.primary">
-                説明
-              </Typography>
-              <Typography variant="body1" color="text.primary">
-                {task.description}
-              </Typography>
-            </Stack>
-            <Stack spacing={0.3}>
-              <Typography variant="body2" color="text.primary">
-                作成者
-              </Typography>
-              <Stack direction="row" spacing={1} alignItems="center">
-                <Avatar
-                  src={PigImage}
-                  css={css`
-                    width: 25px;
-                    height: 25px;
-                  `}
+        {(!showMyTasks || task.order_user_id === user?.id) && (
+          <Grid item xs={2}>
+            <Stack
+              spacing={0.5}
+              css={css`
+                align-items: end;
+                padding-right: 10px;
+              `}
+            >
+              <>
+                <EditTask
+                  setTasks={setTasks}
+                  task={task}
+                  friends={friends}
+                  setFriends={setFriends}
+                  showMyTasks={showMyTasks}
                 />
-                <Stack direction="column" spacing={0} alignItems="start">
-                  <Typography variant="body1" color="text.primary">
-                    {task.order_user.name}
-                  </Typography>
-                  <Typography variant="body2" color="text.secondary">
-                    {task.order_user.email}
-                  </Typography>
+                <DeleteTask
+                  setTasks={setTasks}
+                  id={task.id}
+                  showMyTasks={showMyTasks}
+                />
+              </>
+            </Stack>
+          </Grid>
+        )}
+        <Grid
+          container
+          css={css`
+            max-height: ${show ? "100vh" : 0};
+            transition: max-height 0.7s, padding 0.5s;
+            overflow: hidden;
+            padding: ${show ? "20px 0 0" : 0};
+          `}
+          onClick={handleClickOpen}
+        >
+          <Grid item xs={2}></Grid>
+          <Grid item xs={10}>
+            <Stack spacing={3}>
+              <Stack spacing={0.3}>
+                <Typography variant="body2" color="text.primary">
+                  説明
+                </Typography>
+                <Typography variant="body1" color="text.primary">
+                  {task.description}
+                </Typography>
+              </Stack>
+              <Stack spacing={0.3}>
+                <Typography variant="body2" color="text.primary">
+                  作成者
+                </Typography>
+                <Stack direction="row" spacing={1} alignItems="center">
+                  <Avatar
+                    src={PigImage}
+                    css={css`
+                      width: 25px;
+                      height: 25px;
+                    `}
+                  />
+                  <Stack direction="column" spacing={0} alignItems="start">
+                    <Typography variant="body1" color="text.primary">
+                      {task.order_user.name}
+                    </Typography>
+                    <Typography variant="body2" color="text.secondary">
+                      {task.order_user.email}
+                    </Typography>
+                  </Stack>
                 </Stack>
               </Stack>
+              <Stack spacing={0.3}>
+                <Typography variant="body2" color="text.primary">
+                  作成日時
+                </Typography>
+                <Typography variant="body1" color="text.primary">
+                  {changeJSTDate(task.created_at)}
+                </Typography>
+              </Stack>
             </Stack>
-            <Stack spacing={0.3}>
-              <Typography variant="body2" color="text.primary">
-                作成日時
-              </Typography>
-              <Typography variant="body1" color="text.primary">
-                {changeJSTDate(task.created_at)}
-              </Typography>
-            </Stack>
-          </Stack>
+          </Grid>
         </Grid>
       </Grid>
     </Grid>
