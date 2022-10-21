2b6660c45ec994a546983c682743a67a76fa9b3d
yuuumbk <hikoki.y@gmail.com>
2022-10-22 00:10:27 +0900

    add: point component design

--- frontend/src/components/ShadowBox.jsx
+++ frontend/src/components/ShadowBox.jsx
@@ -1,11 +1,11 @@
 import { Container } from "@mui/material";
 import { css } from "@emotion/react";
 
-const ShadowBox = ({ children }) => {
+const ShadowBox = ({ bgColor = "#fff", children }) => {
   return (
     <Container
       css={css`
-        background-color: #fff;
+        background-color: ${bgColor};
         box-shadow: rgba(0, 0, 0, 0.24) 0px 3px 8px;
         border-radius: 15px;
         max-width: 90%;
